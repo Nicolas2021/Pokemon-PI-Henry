d3a632327529ce58a5a99b6f09903bb73fb996f0
Nicolas2021 <doteroutn2020@hotmail.com>
2022-03-11 20:02:30 -0300

    Feat: Default Proyect

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,22 @@
-import './App.css';
-import { Route, Routes } from 'react-router-dom';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
 
-import LandingPage from './components/LandingPage.jsx';
+import LandingPage from "./pages/LandingPage/LandingPage";
+import store from "./redux/store";
+import { Provider } from "react-redux";
+import Home from "./pages/Home/Home";
+import Form from "./pages/Form/Form";
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route exact path= "/" element={<LandingPage/>}/>
-      </Routes>
+      <Provider store={store}>
+        <Routes>
+          <Route exact path="/" element={<LandingPage />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/CreatePokemon" element={<Form />} />
+        </Routes>
+      </Provider>
     </div>
   );
 }
