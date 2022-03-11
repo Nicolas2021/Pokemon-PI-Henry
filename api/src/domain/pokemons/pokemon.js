212a6d5605e52864d18d0809017141dac97a2376
Nicolas2021 <doteroutn2020@hotmail.com>
2022-03-11 20:11:30 -0300

    Feat: FunctionsControllers

--- api/src/domain/pokemons/pokemon.js
+++ api/src/domain/pokemons/pokemon.js
@@ -1,33 +1,33 @@
-const { arrOfPokemonsOfBd } = require("../common/constantes")
-
 function createMyPokemonsResponseFromApi(pokemonsArr) {
-    //nombre img tipo
-    const miInfoDePokemonsDeseada = pokemonsArr.map(pokemonInfoFromApi => {
-         return {
-            id:pokemonInfoFromApi.data.id,
-            nombre: pokemonInfoFromApi.data.name,
-            img: pokemonInfoFromApi.data.sprites.front_default,
-            tipos: pokemonInfoFromApi.data.types.map(type => type.type.name),
-        }
-    })
+  //nombre img tipo
+  const miInfoDePokemonsDeseada = pokemonsArr.map((pokemonInfoFromApi) => {
+    return {
+      id: pokemonInfoFromApi.data.id,
+      nombre: pokemonInfoFromApi.data.name,
+      img: pokemonInfoFromApi.data.sprites.front_default,
+      tipos: pokemonInfoFromApi.data.types.map((type) => type.type.name),
+    };
+  });
 
-    return miInfoDePokemonsDeseada
-}
-function createMyPokemonsResponseFromBd(pokemonsArr) {
-    //nombre img tipo
-    const miInfoDePokemonsDeseada = pokemonsArr.map(pokemonInfoFromApi => {
-         return {
-            id:pokemonInfoFromApi.id,
-            nombre: pokemonInfoFromApi.nombre,
-            img: pokemonInfoFromApi.img,
-            tipos: pokemonInfoFromApi.tipos.map(type => type.type.name)
-        }
-    })
-
-    return miInfoDePokemonsDeseada
-}
-function getPokemonsByBd() {
-    return arrOfPokemonsOfBd;
+  return miInfoDePokemonsDeseada;
 }
 
-module.exports = {createMyPokemonsResponseFromApi,getPokemonsByBd,createMyPokemonsResponseFromBd}
+function pokemonsFromBD(pokemonsData) {
+  const pokemonsInfo = pokemonsData.map((el) => {
+    return {
+      id: el.dataValues.id.slice(0, 3),
+      nombre: el.dataValues.nombre,
+      vida: el.dataValues.vida,
+      fuerza: el.dataValues.fuerza,
+      defensa: el.dataValues.defensa,
+      velocidad: el.dataValues.velocidad,
+      altura: el.dataValues.altura,
+      peso: el.dataValues.peso,
+      img: el.dataValues.img,
+      tipos: el.dataValues.tipos.map((el) => el.nombre),
+    };
+  });
+  return pokemonsInfo;
+}
+
+module.exports = { createMyPokemonsResponseFromApi, pokemonsFromBD };
