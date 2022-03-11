9a7ac18ebc12d65d894bea9618ab6ded2757bd5c
Nicolas2021 <doteroutn2020@hotmail.com>
2022-03-11 20:04:37 -0300

    Feat: Models DB

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -1,10 +1,43 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('pokemon', {
-    name: {
+  sequelize.define("pokemon", {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      allowNull: true,
+    },
+    nombre: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    vida: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    fuerza: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    defensa: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    velocidad: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    altura: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    peso: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    img: {
       type: DataTypes.STRING,
       allowNull: false,
     },
