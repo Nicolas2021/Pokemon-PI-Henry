43776b1d38ccb7d895d56354c739f8a4cdaafdef
Nicolas2021 <doteroutn2020@hotmail.com>
2022-03-16 19:50:04 -0300

    Feat: Form Page

--- client/src/pages/Form/Form.jsx
+++ client/src/pages/Form/Form.jsx
@@ -4,6 +4,7 @@ import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
 import { getAllTypes } from "../../redux/actions/pokemonAction";
 import axios from "axios";
+import { Link } from "react-router-dom";
 import { posteoPokemons } from "../../assets/common/configurarRutas";
 
 import "./Form.css";
@@ -16,30 +17,34 @@ const Forms = () => {
   //States----
 
   const tipo = useSelector((state) => state.tipos);
+  const [botonActivo, setBotonActivo] = useState(false);
   const [tipos, setTipos] = useState([]);
   const [error, setError] = useState({
     nombre: "",
     vida: "",
-    ataque: "",
+    fuerza: "",
   });
   const [pokemon, setPokemon] = useState({
     nombre: "",
     vida: "",
-    ataque: "",
+    fuerza: "",
     defensa: "",
     velocidad: "",
     altura: "",
     peso: "",
-    imagen: "",
+    img: "",
     tipo: tipos,
   });
 
   //Functions-----
+
   function validate(el) {
     if (pokemon[el] === "") {
       setError({ ...error, [el]: `El campo ${el} no puede estar vacio` });
+      setBotonActivo(false);
     } else {
       setError({ ...error, [el]: "" });
+      setBotonActivo(true);
     }
   }
 
@@ -62,94 +67,115 @@ const Forms = () => {
   return (
     <section>
       <form className="form__Pokemon" onSubmit={handleSubmit}>
-        <h1>Carga tu Pokemon</h1>
+        <div className="container__form">
+          <div className="side__top">
+            <h1>Carga tu Pokemon</h1>
+          </div>
+          <div className="continainer__sides">
+            <div className="side__left">
+              <label htmlFor="inputnombre">Nombre:</label>
+              <input
+                className={error.nombre ? "danger" : "input-Form"}
+                type="text"
+                name="nombre"
+                id="inputnombre"
+                value={pokemon.nombre}
+                onBlur={(e) => validate(e.target.name)}
+                onChange={onInputChange}
+                autoFocus
+              />
+              {!error.nombre ? null : (
+                <span className="spanAlert">{error.nombre}</span>
+              )}
+              <br />
+              <label htmlFor="">Vida:</label>
+              <input
+                className={error.vida ? "danger" : "input-Form"}
+                type="number"
+                name="vida"
+                value={pokemon.vida}
+                onBlur={(e) => validate(e.target.name)}
+                onChange={onInputChange}
+              />
+              {!error.vida ? null : (
+                <span className="spanAlert">{error.vida}</span>
+              )}
+              <br />
+              <label htmlFor="">Ataque</label>
+              <input
+                className={error.fuerza ? "danger" : "input-Form"}
+                type="number"
+                name="fuerza"
+                value={pokemon.fuerza}
+                onBlur={(e) => validate(e.target.name)}
+                onChange={onInputChange}
+              />
+              {!error.fuerza ? null : (
+                <span className="spanAlert">{error.fuerza}</span>
+              )}
+              <br />
+              <label htmlFor="">Defensa:</label>
+              <input
+                type="number"
+                name="defensa"
+                value={pokemon.defensa}
+                onChange={onInputChange}
+              />
+            </div>
+            <div className="side__right">
+              <label htmlFor="">Velocidad:</label>
+              <input
+                type="number"
+                name="velocidad"
+                value={pokemon.velocidad}
+                onChange={onInputChange}
+              />
+              <br />
 
-        <label htmlFor="inputnombre">Nombre:</label>
-        <input
-          className={error.nombre ? "danger" : "input-Form"}
-          type="text"
-          name="nombre"
-          id="inputnombre"
-          value={pokemon.nombre}
-          onBlur={(e) => validate(e.target.name)}
-          onChange={onInputChange}
-        />
-        {!error.nombre ? null : (
-          <span className="spanAlert">{error.nombre}</span>
-        )}
+              <label htmlFor="">Altura:</label>
+              <input
+                type="number"
+                name="altura"
+                value={pokemon.altura}
+                onChange={onInputChange}
+              />
+              <br />
 
-        <label htmlFor="">Vida:</label>
-        <input
-          className={error.vida ? "danger" : "input-Form"}
-          type="number"
-          name="vida"
-          value={pokemon.vida}
-          onBlur={(e) => validate(e.target.name)}
-          onChange={onInputChange}
-        />
-        {!error.vida ? null : <span className="spanAlert">{error.vida}</span>}
-
-        <label htmlFor="">Ataque</label>
-        <input
-          className={error.ataque ? "danger" : "input-Form"}
-          type="number"
-          name="ataque"
-          value={pokemon.ataque}
-          onBlur={(e) => validate(e.target.name)}
-          onChange={onInputChange}
-        />
-        {!error.ataque ? null : (
-          <span className="spanAlert">{error.ataque}</span>
-        )}
-
-        <label htmlFor="">Defensa:</label>
-        <input
-          type="number"
-          name="defensa"
-          value={pokemon.defensa}
-          onChange={onInputChange}
-        />
-
-        <label htmlFor="">Velocidad:</label>
-        <input
-          type="number"
-          name="velocidad"
-          value={pokemon.velocidad}
-          onChange={onInputChange}
-        />
-
-        <label htmlFor="">Altura:</label>
-        <input
-          type="number"
-          name="altura"
-          value={pokemon.altura}
-          onChange={onInputChange}
-        />
-
-        <label htmlFor="">Peso:</label>
-        <input
-          type="number"
-          name="peso"
-          value={pokemon.peso}
-          onChange={onInputChange}
-        />
-
-        <label htmlFor="">Imagen:</label>
-        <input
-          type="file"
-          name="imagen"
-          value={pokemon.imagen}
-          onChange={onInputChange}
-        />
-
-        <label htmlFor="">Tipos:</label>
-        <select name="tipo" id="" onChange={onInputTypes}>
-          {tipo.map((el) => {
-            return <option value={el.name}>{el.name} </option>;
-          })}
-        </select>
-
-        <input type="submit" onClick={(e) => validate(e.target.name)} />
+              <label htmlFor="">Peso:</label>
+              <input
+                type="number"
+                name="peso"
+                value={pokemon.peso}
+                onChange={onInputChange}
+              />
+              <br />
+              <label htmlFor="">Tipos:</label>
+              <select name="tipo" id="" onChange={onInputTypes} form="carform">
+                {tipo.map((el, item) => {
+                  return (
+                    <option key={item} value={el.name}>
+                      {el.name}{" "}
+                    </option>
+                  );
+                })}
+              </select>
+            </div>
+          </div>
+          <div className="side__bottom">
+            <br />
+            <div className="bottom__buttons">
+              <Link to="/home">
+                <button className="back__button">Volver</button>
+              </Link>
+              <input
+                className="submit__button"
+                type="submit"
+                onClick={(e) => validate(e.target.name)}
+                disabled={!botonActivo}
+              />
+            </div>
+          </div>
+        </div>
       </form>
     </section>
   );
