a3fc98c53c50afb7e277b005304966039cb597ee
Nicolas2021 <doteroutn2020@hotmail.com>
2022-03-18 17:48:27 -0300

    Fix: Modal Req Data

--- client/src/components/Modal/Modal.jsx
+++ client/src/components/Modal/Modal.jsx
@@ -5,14 +5,14 @@ import "./Modal.css";
 
 export const Modal = ({ closeModal, numeroId }) => {
   const dispatch = useDispatch();
-  var pokemonUnic = useSelector((state) => state.id);
-  const { nombre, vida, fuerza, defensa, velocidad, altura, peso, img, tipos } =
-    pokemonUnic[0];
-  //-------------------------------------
   useEffect(() => {
     dispatch(getById(numeroId));
   }, [dispatch]);
+  //-------------------------------------
+  var pokemonUnic = useSelector((state) => state.id);
 
+  /*  const { nombre, vida, fuerza, defensa, velocidad, altura, peso, img, tipos } =
+    pokemonUnic[0]; */
   //-------------------------------------
   function onCloseModal() {
     pokemonUnic[0] = [];
@@ -28,16 +28,16 @@ export const Modal = ({ closeModal, numeroId }) => {
           <button onClick={() => onCloseModal()}>X</button>
         </div>
         <div className="modal__body">
-          <img src={img} alt="" />
+          <img src={pokemonUnic[0]?.img} alt="" />
           <div className="modal__info">
-            <p>Nombre:{nombre} </p>
-            <p>Vida:{vida} </p>
-            <p>Fuerza:{fuerza} </p>
-            <p>Defensa:{defensa} </p>
-            <p>Velocidad:{velocidad} </p>
-            <p>Altura:{altura} </p>
-            <p>Peso:{peso} </p>
-            <p>Tipo:{tipos} </p>
+            <p>Nombre:{pokemonUnic[0]?.nombre} </p>
+            <p>Vida:{pokemonUnic[0]?.vida} </p>
+            <p>Fuerza:{pokemonUnic[0]?.fuerza} </p>
+            <p>Defensa:{pokemonUnic[0]?.defensa} </p>
+            <p>Velocidad:{pokemonUnic[0]?.velocidad} </p>
+            <p>Altura:{pokemonUnic[0]?.altura} </p>
+            <p>Peso:{pokemonUnic[0]?.peso} </p>
+            <p>Tipo:{pokemonUnic[0]?.tipos} </p>
           </div>
         </div>
       </div>
