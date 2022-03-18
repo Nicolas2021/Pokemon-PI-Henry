81fb7bef56f488ddda98cb9da209053409f8e531
Nicolas2021 <doteroutn2020@hotmail.com>
2022-03-18 17:49:09 -0300

    Feat: Search By Name

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,13 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { getAllPokemons, getName } from "../../redux/actions/pokemonAction";
 import "./NavBar.css";
 
 export const NavBar = () => {
+  const [search, setSearch] = useState("");
+  const dispatch = useDispatch();
+
+  function onHandleChange(e) {
+    setSearch(e.target.value);
+  }
+
+  function onClickHandle() {
+    dispatch(getName(search));
+  }
+
+  function onAbortChange() {
+    dispatch(getAllPokemons());
+  }
+
   return (
     <div className="navContainer">
       <h1>Pokemon-PI/Henry</h1>
       <div className="search__button">
-        <input type="search" placeholder="...Search Pokemon" />
-        <button></button>
+        <input
+          type="search"
+          placeholder="...Search Pokemon"
+          onChange={onHandleChange}
+          onAbort={onAbortChange}
+        />
+        <button type="submit" onClick={onClickHandle}></button>
       </div>
     </div>
   );
